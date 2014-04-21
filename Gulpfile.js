e484d6ee12f21e212e9b2b9e3bb2a0a9a206a70c
Carlos Guzman <carlos.guzman@naranya.com>
2014-04-21 12:35:03 -0500

    Revision de paquetes en package.json

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,28 +1,23 @@
 var gulp = require('gulp');
 var jade = require('gulp-jade');
 var stylus = require('gulp-stylus');
-var plumber = require('gulp-plumber');
 var watch = require('gulp-watch');
-var gutil = require('gulp-util');
-
-var onError = function (err) {  
-	gutil.beep();
-	console.log(err);
-};
+var connect = require('connect');
 
 gulp.task('default', function () {
 	gulp.src('./jade/*.jade')
 		.pipe(watch(function(files){
-			return files.pipe(plumber({ errorHandler: onError })).pipe( jade({ pretty: true }) ).pipe( gulp.dest('./public/') );
+			return files.pipe( jade({ pretty: true }) )
+						.pipe( gulp.dest('./public/') );
 		}));
 
 	gulp.src('./stylus/*.styl')
 		.pipe(watch(function(files){
-			return files.pipe(plumber({ errorHandler: onError })).pipe( stylus() ).pipe( gulp.dest('./public/css/') );
+			return files.pipe( stylus() )
+						.pipe( gulp.dest('./public/css/') );
 		}));
-
-	var connect = require('connect');
-	connect.createServer(
-		connect.static(__dirname)
-	).listen(8080);
 });
+
+connect.createServer(
+	connect.static(__dirname)
+).listen(8080);
